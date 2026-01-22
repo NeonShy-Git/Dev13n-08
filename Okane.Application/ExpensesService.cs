b19a39de45bcdd8cc6a65a653a5f82095e212a13
orodriguez <omarjavier15@gmail.com>
2026-01-22 11:20:07 -0400

    Make build fail on purpose

--- Okane.Application/ExpensesService.cs
+++ Okane.Application/ExpensesService.cs
@@ -2,7 +2,7 @@ namespace Okane.Application;
 
 public class ExpensesService(List<Expense> expenses)
 {
-    private static int _lastId = 1;
+    private static int _lastId = 10;
 
     public Expense Create(CreateExpenseRequest request)
     {
